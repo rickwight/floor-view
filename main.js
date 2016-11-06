6829e4965a1e6a3aab6b6ddcb198f2f06f22aba2
Rick Wight <rick.wight@gmail.com>
2016-11-06 13:01:05 -0800

    inital commit of version 2. use visjs library, add grpahing styles

--- main.js
+++ main.js
@@ -5,84 +5,33 @@ function drawGraph() {
 
   console.log('draw: ' + width + 'x' + height + ' points: ' + points.length);
 
-  var graph = d3.gl.graph()
-    .ortho(window.ortho)
-    .width(width)
-    .height(height);
+  var dim = Math.min(width, height);
 
-  var colourRange = d3.scale.linear()
-    .domain([
-        window.boundLower.z,
-        (window.boundLower.z - window.boundLower.z / 2),
-        window.boundUpper.z
-      ])
-    .range(["red", "green", "red"]);
-
-  var pointcloud = graph.points()
-    .data(points)
-    .size(3)
-    .color(function(d) {
-      return colourRange(d.z);
-    });
-
-  var scales = {
-    "x": d3.scale.linear().domain([window.boundLower.x, window.boundUpper.x]),
-    "y": d3.scale.linear().domain([window.boundLower.y, window.boundUpper.y]),
-    "z": d3.scale.linear().domain([window.boundLower.z, window.boundUpper.z])
+  // specify options
+  var options = {
+      width: "" + dim + "px",
+      height: "" + dim + "px",
+      style: window.graph_style,
+      showPerspective: !window.ortho,
+      showGrid: true,
+      showShadow: false,
+      keepAspectRatio: true,
+      dotSizeRatio: 0.003,
+      verticalRatio: 0.5
   };
 
-  var axis = graph.axis()
-    .data(["x", "y", "z"])
-    .scale(function(d) { return scales[d]; })
-    .orient(function(d) { return d; })
-    .thickness(1)
-    .offset(function(d) {
-      if ("x" == d) {
-        return makePoint(0, window.boundLower.y, window.boundLower.z);
-      } else if ("y" == d) {
-        return makePoint(window.boundLower.x, 0, window.boundLower.z);
-      } else if ("z" == d) {
-        return makePoint(window.boundLower.x, window.boundLower.y, 0);
-      }
-      return makePoint(0,0,0);
-    })
-    .color("#8cc");
+  var useCameraPosition = typeof window.graph != "undefined";
+  var container = document.getElementById("the-graph");
 
-  var ticks = axis.ticks()
-    .count(5)
-    .font("16px Arial")
-    .size(50)
-    .resolution(64)
-    .color("#8cc");
-
-  var label = axis.label()
-    .size(100)
-    .resolution(128)
-    .font("bold 20px Helvetica")
-    .color("#8cc");
-
-  if (window.ortho) {
-    graph.orient(getOrientArray(window.orient));
-    graph.zoom(1.5);
-  } else {
-    graph.orient('x');
-    graph.zoom(4);
+  var cameraPosition;
+  if (useCameraPosition) {
+    cameraPosition = window.graph.getCameraPosition();
   }
 
-  var the_graph = d3.select("#the-graph")[0][0];
-  if (the_graph.childNodes.length > 0) {
-    d3.selectAll(the_graph.childNodes).remove();
-  }
-  d3.select("#the-graph").call(graph);
-}
+  window.graph = new vis.Graph3d(container, points, options);
 
-function getOrientArray(orient) {
-  if (orient == 'x') {
-    return [90, 0, 270];
-  } else if (orient == 'y') {
-    return [90, 0, 180];
-  } else {
-    return [0, 0, 270];
+  if (useCameraPosition) {
+    window.graph.setCameraPosition(cameraPosition);
   }
 }
 
@@ -103,75 +52,6 @@ function parseInput(input) {
   return new_points;
 }
 
-function makePoint(x, y, z) {
-  return {'x': x, 'y': y, 'z': z };
-}
-
-function findBounds(points) {
-  var upper = $.extend({}, points[0]);
-  var lower = $.extend({}, points[0]);
-
-  for (var p = 0; p < points.length; p++) {
-    upper.x = points[p].x > upper.x ? points[p].x : upper.x;
-    upper.y = points[p].y > upper.y ? points[p].y : upper.y;
-    upper.z = points[p].z > upper.z ? points[p].z : upper.z;
-
-    lower.x = points[p].x < lower.x ? points[p].x : lower.x;
-    lower.y = points[p].y < lower.y ? points[p].y : lower.y;
-    lower.z = points[p].z < lower.z ? points[p].z : lower.z;
-  }
-
-  window.boundUpper = upper;
-  window.boundLower = lower;
-}
-
-function transformPoints(points) {
-  var translate = {
-    'x' : 0,
-    'y' : 0,
-    'z' : 0,
-  }
-
-  window.scaleFactor = calculateScaleFactor(window.boundUpper.x - window.boundLower.x);
-  var scale = makePoint(
-      window.scaleFactor,
-      window.scaleFactor,
-      window.scaleFactor);
-
-  for (var p = 0; p < points.length; p++) {
-    points[p] = transformPoint(points[p], scale, translate);
-  }
-
-  return points;
-}
-
-function calculateScaleFactor(diff) {
-  var scale = 1;
-  var found = false;
-  while (!found) {
-    if (diff * scale < 10) {
-      scale *= 10;
-    } else if (diff * scale > 100) {
-      scale *= 0.1;
-    } else {
-      found = true;
-    }
-  }
-  return scale;
-}
-
-function transformPoint(point, scale, translate) {
-  point.x *= scale.x;
-  point.y *= scale.y;
-  point.z *= scale.z;
-
-  point.x += translate.x;
-  point.y += translate.y;
-  point.z += translate.z;
-
-  return point;
-}
-
 function printPoints(points) {
   for (var i = 0; i < points.length; i++) {
     printPoint(points[i]);
@@ -182,15 +62,9 @@ function printPoint(point) {
   console.log("" + point.x + "," + point.y + "," + point.z);
 }
 
-function time_ms() {
-  return new Date().getTime();
-}
 
 function initPoints() {
-  points = parseInput($("#the-input-textarea").val());
-  findBounds(points);
-  points = transformPoints(points);
-  findBounds(points);
+  var points = parseInput($("#the-input-textarea").val());
   window.points = points;
 }
 
@@ -202,11 +76,10 @@ function updateGraph() {
 function genCsvPoints() {
   var x, y, z;
   var str = "";
-  for (x = 0; x < 10; x++) {
-    for (y = 0; y < 10; y++) {
-      for (z = 0; z < 4; z++) {
-        str += ("" + x + "," + y + "," + z + "\n")
-      }
+  var res = 20;
+  for (x = 0; x < res; x++) {
+    for (y = 0; y < res; y++) {
+      str += ("" + x + "," + y + "," + Math.random() + "\n")
     }
   }
   return str;
@@ -216,6 +89,7 @@ $(document).ready(function() {
   window.ortho = false;
   window.hidePanel = false;
   window.orient = 'x';
+  window.graph_style = 'surface';
   $("#the-input-textarea").val(genCsvPoints());
   $("#the-input").draggable();
   updatePanel();
@@ -236,12 +110,6 @@ function updatePanel() {
     $("#the-input").find(".hidable").removeClass("hide");
     $("#the-input-hide-button").html("Hide");
   }
-
-  if (window.ortho) {
-    $("#the-input").find(".ortho-hidable").removeClass("hide");
-  } else {
-    $("#the-input").find(".ortho-hidable").addClass("hide");
-  }
 }
 
 $("#the-graph-button").click(function() {
@@ -259,16 +127,16 @@ $("#the-ortho-checkbox").click(function() {
   drawGraph();
 });
 
-$("#the-x-button").click(function() {
-  window.orient = 'x';
+$("#the-style-dot-button").click(function() {
+  window.graph_style = 'dot';
   drawGraph();
 });
-$("#the-y-button").click(function() {
-  window.orient = 'y';
+$("#the-style-grid-button").click(function() {
+  window.graph_style = 'grid';
   drawGraph();
 });
-$("#the-z-button").click(function() {
-  window.orient = 'z';
+$("#the-style-surface-button").click(function() {
+  window.graph_style = 'surface';
   drawGraph();
 });
 
